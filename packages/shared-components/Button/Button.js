6def709174beca00651214d4d4b5865e3469e534
shyam <mvmshyam@gmail.com>
2021-09-14 19:15:46 +0530

    init

---
+++ packages/shared-components/Button/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Button(props) {
+  return <div>Button component from shared module</div>;
+}
+
+export default Button;
---
+++ packages/shared-components/Input/Input.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Input(props) {
+  return <div>Input component</div>;
+}
+
+Input.propTypes = {};
+
+export default Input;
